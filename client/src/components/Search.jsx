9b2cc33794905091c16e2746b4d8133755013369
Matt Waelder <mattwaelder@gmail.com>
2022-08-15 12:34:03 -0700

    initial commit

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -18,12 +18,14 @@ class Search extends React.Component {
     this.props.onSearch(this.state.term);
   }
 
+  //i added onclick callback fn and .bind(this);
+
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
-    </div>) 
+      Enter a github username: <input value={this.state.term} onChange={this.onChange.bind(this)}/>
+      <button onClick={() => this.search}> Add Repos </button>
+    </div>)
   }
 }
 
--- database/index.js
+++ database/index.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+//make mongoose schema
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  username: String,
+  repos: Array,
 });
 
+//compile mongoose schema into model
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
+//can add methods to method prop of mongoose model (classes)
+//eg a method for pulling "top" 25 repos?
+//this.getTopRepos() ??
+
+
+//each document (a class based on schema) has a save method
+//which takes err 1st and saves to db
+let save = (err, data) => {
   // This function should save a repo or repos to
   // the MongoDB
+  if (err) {
+    console.log('failed to write', err)
+  } else {
+    console.log('database updated with', data)
+  }
+
 }
 
 module.exports.save = save;
